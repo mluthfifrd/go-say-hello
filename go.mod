30b9f94c39524947a65a781daf1f5085bd2caf3c
mluthfifrd <82927435+mluthfifrd@users.noreply.github.com>
2025-10-21 22:10:26 +0700

    update say hello function with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mluthfifrd/go-say-hello
+module github.com/mluthfifrd/go-say-hello/v2
 
 go 1.25.2
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello Golang"
+func SayHello(name string) string {
+	return "Hello Golang " + name
 }
